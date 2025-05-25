9ba0f32ec00d5c9a74e8aba8f908b1359e32eae7
Ola1nfo <t-baiunm@mon.gov.ua>
2025-05-25 21:48:03 +0300

    NotFound button back home

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,5 +1,7 @@
 import superman from '../NotFound/img/superman.gif'
 import clouds from '../NotFound/img/clouds.png'
+import funny from '../NotFound/img/funny.png'
+import {Link} from 'react-router'
 import './NotFound.css'
 
 export default function NotFound() {
@@ -30,7 +32,7 @@ export default function NotFound() {
                     <p className='text1'>Схоже, щось пішло не так...</p>
                     <p className='text2'>Але не засмучуйтесь — Супермен уже летить вам на допомогу!</p>
                 </div>
-                <p className='text3'>Ви можете пошукати потрібну інформацію <strong>тут</strong>. А якщо вам тут сподобалося — повертайтесь ще! За адресою, яку самі вигадаєте</p>
+                <p className='text3'>Ви можете пошукати потрібну інформацію <strong><Link className='link' to="/">тут</Link></strong>. А якщо вам тут сподобалося — повертайтесь ще! За адресою, яку самі вигадаєте <img className='funny' src={funny} alt="funny" /></p>
             </div>
         </div>
         
